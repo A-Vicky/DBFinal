188c0aacefbd9e8050b5344b878a3af956d73cda
A-Vicky <45051911+A-Vicky@users.noreply.github.com>
2020-06-12 17:58:25 -0600

    tablas de pocos datos sistema c

---
+++ guardarpocos.sql
@@ -0,0 +1,23 @@
+insert into country (name) values ('Guatemala');
+insert into country (name) values ('El Salvador');
+insert into country (name) values ('Honduras');
+insert into country (name) values ('Mexico');
+insert into country (name) values ('Estados Unidos');
+insert into country (name) values ('Belize');
+
+insert into jobposition (name) values ('Gerente');
+insert into jobposition (name) values ('Asistente de IT');
+insert into jobposition (name) values ('Cajero');
+insert into jobposition (name) values ('Servicio al Cliente');
+
+insert into typeornato (Cost, Salaryrange) values (10,'500 - 1000');
+insert into typeornato (Cost, Salaryrange) values (15,'1001 - 3000');
+insert into typeornato (Cost, Salaryrange) values (50,'3001 - 6000');
+insert into typeornato (Cost, Salaryrange) values (75,'6001 - 9000');
+insert into typeornato (Cost, Salaryrange) values (100,'9001 - 12000');
+insert into typeornato (Cost, Salaryrange) values (150,'12001 en adelante');
+
+insert into typepay (Type) values ('Certificado');
+insert into typepay (Type) values ('Ornato');
+insert into typepay (Type) values ('Otro');
+
